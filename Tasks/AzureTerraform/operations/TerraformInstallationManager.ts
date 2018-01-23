7060ad1389da8f09a438557a56e405fa2affcbac
Jacob Cleaveland <bluemoomba@gmail.com>
2018-01-23 00:44:39 -0800

    Started working on getting the Terraform executable.

---
+++ Tasks/AzureTerraform/operations/TerraformInstallationManager.ts
@@ -0,0 +1,37 @@
+import * as httpm from 'typed-rest-client/HttpClient';
+import * as cheerio from 'cheerio';
+import { version } from 'punycode';
+import MachineCharacteristics from '../models/MachineCharacteristics';
+
+let httpc: httpm.HttpClient = new httpm.HttpClient('vsts-node-api');
+
+export enum OS {
+
+}
+
+export default class TerraformInstallationManager {
+    private machineDetails: MachineCharacteristics;
+    
+    constructor(machineData: MachineCharacteristics) {
+        this.machineDetails = machineData;
+    }
+
+    public async downloadTerraform() {
+        let versionsResponse: httpm.HttpClientResponse = await httpc.get('https://releases.hashicorp.com/terraform');
+
+        let versions: Array<string> = [];
+        let parser = cheerio.load(await versionsResponse.readBody());
+        parser('a').each((i, element) => {
+            let content: string = parser(element).text();
+            if(content.match(/terraform/) && !content.match(/rc|beta/))
+                versions.push(content);
+        });
+        let version: string = versions[0].replace('terraform_', '');
+
+        let exeResponse: httpm.HttpClientResponse = await httpc.get("https://releases.hashicorp.com/terraform/" + version);
+        
+        let downloadLink: string = '';
+        parser = cheerio.load(await exeResponse.readBody());
+        parser('a').f
+    }
+}
--- Tasks/AzureTerraform/operations/TerraformRun.ts
+++ Tasks/AzureTerraform/operations/TerraformRun.ts
@@ -7,5 +7,17 @@ export default class TerraformRun {
         this.taskParams = params;
     }
 
-    
+    public async execute() : Promise<void> {
+        // Steps:
+        // - Find, download, update, and/or install Terraform.
+        // - Handle backend operations (if requested).
+        //     - Get backend params.
+        //     - Get backend params.
+        // - Handle AzureCLI operations (if requested).
+        //     - Get AzureCLI params.
+        //     - Log into AzureCLI.
+        // 
+
+        return undefined;
+    }
 }
